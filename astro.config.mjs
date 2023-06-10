ff4053ae27681da46e28cc42e3adbca091fa3ce5
kim <manmen.mi1375@gmail.com>
2023-06-10 18:44:39 +0900

    路線変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
-import prefetch from "@astrojs/prefetch";
-
-import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), prefetch(), solidJs()],
+  integrations: [tailwind(), image()],
   site: 'https://manmen-mi.github.io',
-  base: '/astro-page',
+  base: '/',
 });
